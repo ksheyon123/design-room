d25c57e92e718cd198dc8759cbcf804f16b30d00
KagnSeohyeon <shkang@coinplug.com>
2024-06-27 13:53:32 +0900

    :package:setup initial nextjs settings

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  /* config options here */
+  pageExtensions: ["ts", "tsx"],
+  reactStrictMode: true,
+  env: {},
+};
+
+export default nextConfig;
